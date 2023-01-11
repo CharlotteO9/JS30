cc7c38585ead5b5a6075bd70b339197b57d373f3
charlotte oswald <charlotteoswald9@gmail.com>
2023-01-11 16:13:16 +0100

    coded drum kit!

--- drum_kit/index.js
+++ drum_kit/index.js
@@ -1,6 +1,4 @@
-
-
-window.addEventListener('keyup', (event)=> {
+const playSound = (event) => {
   // console.log(event);
 
   const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
@@ -11,5 +9,21 @@ window.addEventListener('keyup', (event)=> {
 
   const letter = document.querySelector(`div[data-key="${event.keyCode}"]`);
   // console.log(letter);
-  letter.classList.add('playing')
+  letter.classList.add('playing');
+}
+
+const removePlaying = (event) => {
+  // console.log(event)
+  if (event.propertyName !== 'transform') {
+    return;
+  }
+  // console.log(event.currentTarget);
+  event.currentTarget.classList.remove('playing');
+};
+
+window.addEventListener('keyup', playSound)
+
+const letters = document.querySelectorAll('.list');
+letters.forEach(letter => {
+  letter.addEventListener('transitionend', removePlaying);
 });
